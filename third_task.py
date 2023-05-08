fb81e34061d03e8ebb9f654e606548d444e7e765
yuliaonisshenko <onischenkoooyulia112gmail.com>
2023-05-08 23:01:49 +0300

    add third task

---
+++ third_task.py
@@ -0,0 +1,7 @@
+#3 task
+casino_blacklist = ['Franklin Gates', 'Jeremy Gallagher', 'Tahlia Cuevas', 'Keane Lane' ]
+poker_blacklist = ['Genevieve Owens', 'Ellena Petty', 'Franklin Gates', 'Shawn Gutierrez', 'Asad Villega', 'Stella Mays']
+alcohol_blacklist = ['Kylie Valdez', 'Lawrence Calhoun', 'Zaina Holland', 'Franklin Gates', 'Anna Payne', 'Ruby Hatfield']
+print(casino_blacklist.count('Franklin Gates'))
+print(poker_blacklist.count('Franklin Gates'))
+print(alcohol_blacklist.count('Franklin Gates'))
