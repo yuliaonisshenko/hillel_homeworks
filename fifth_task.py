43015395a59f7ee2e9e080926119decd144e2ea5
yuliaonisshenko <onischenkoooyulia112gmail.com>
2023-05-08 23:03:58 +0300

    add fifth task

---
+++ fifth_task.py
@@ -0,0 +1,7 @@
+#5 task
+vip_guests = ['Juli', 'Michael', 'John']
+common_guests = ['Ann', 'Kate', 'Max']
+print('Guests on the VIP seats:')
+print(",".join(vip_guests))
+print('Guests on the usual seats:')
+print(",".join(common_guests))
