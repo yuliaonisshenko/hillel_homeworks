50a37089daff1714b1616ad431d1eb438ac741ea
yuliaonisshenko <onischenkoooyulia112gmail.com>
2023-05-08 19:36:03 +0300

    test

--- second_task.py
+++
@@ -1,6 +0,0 @@
-#2 task
-eleks_company = ['Maria', 'Max', 'Julia', 'Peter']
-print("Eleks company:", eleks_company)
-toshiba_company = ['Marta', 'Michael', 'Nick']
-toshiba_company.append(eleks_company)
-print("Toshiba company:", toshiba_company)
