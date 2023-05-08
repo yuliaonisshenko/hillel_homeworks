2915a7f1f71cb1e1d230d519be158594d2aa54af
yuliaonisshenko <onischenkoooyulia112gmail.com>
2023-05-08 23:00:17 +0300

    add second task

--- second_task.py
+++ second_task.py
@@ -1,1 +1,7 @@
+#2 task
+eleks_company = ['Maria', 'Max', 'Julia', 'Peter']
+print("Eleks company:", eleks_company)
+toshiba_company = ['Marta', 'Michael', 'Nick']
+toshiba_company.append(eleks_company)
+print("Toshiba company:", toshiba_company)
 
