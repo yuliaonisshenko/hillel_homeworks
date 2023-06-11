8ebc709c0d796b15f61a956f4db8593beb0215ba
yuliaonisshenko <onischenkoooyulia112gmail.com>
2023-06-11 16:30:11 +0300

    fixed all function as not default

--- seventhHW/fifth_task.py
+++ seventhHW/fifth_task.py
@@ -2,10 +2,16 @@
 from typing import Callable, Iterable, TypeVar
 
 T = TypeVar('T')
+
 def created_all(sequence: Iterable[T], callback: Callable[[T], bool]) -> bool:
-    return all(callback(item) for item in sequence)
+    for item in sequence:
+        if not callback(item):
+            return False
+    return True
+
 def is_positive(x: T) -> bool:
     return x > 0
+
 numbers = [1, 2, 3, 4, 5]
 result = created_all(numbers, is_positive)
 print(result)
