52f607b8a7940a479038e9400ac6ede01a86fe69
yuliaonisshenko <onischenkoooyulia112gmail.com>
2023-07-03 17:20:56 +0300

    fixed 9hw

--- ninethHW/worker.py
+++ ninethHW/worker.py
@@ -1,9 +1,9 @@
 class Worker:
     def __init__(self, name: str, position: str):
-        self._name = None
-        self._position = None
-        self.name = name
-        self.position = position
+        self._name = None  # Initialize name attribute
+        self._position = None  # Initialize position attribute
+        self.name = name  # Set name using property setter
+        self.position = position  # Set position using property setter
 
     @property
     def name(self) -> str:
@@ -13,7 +13,7 @@ class Worker:
     def name(self, value: str) -> None:
         if not isinstance(value, str):
             raise ValueError("Name must be a string.")
-        self._name = value
+        self._name = value  # Set name attribute
 
     @property
     def position(self) -> str:
@@ -23,4 +23,4 @@ class Worker:
     def position(self, value: str) -> None:
         if not isinstance(value, str):
             raise ValueError("Position must be a string.")
-        self._position = value
+        self._position = value  # Set position attribute
